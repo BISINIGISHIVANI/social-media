3ea54a6dc451d77fef589004c4c6947d5459ecaf
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-08-07 11:22:29 +0530

    feat:add edit postcard

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 import { Navbar } from "./navbar/navbar";
 import { Sidebar } from "./sidebar/sidebar";
-export {Navbar,Sidebar}
+import { PostCard } from "./postCard/postCard";
+import { PostModal } from "./postModal/postModal";
+import { AddPost } from "./addPost/addPost";
+export {Navbar,Sidebar,PostCard,PostModal,AddPost}
