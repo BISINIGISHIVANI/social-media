cfe92ee1a44989878a0fa7c1120e31c38b2ce53a
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-08-09 09:26:38 +0530

    feat:update userThunk ,userslice with follow/unfollow user

--- src/redux/asyncThunk/userThunk.js
+++ src/redux/asyncThunk/userThunk.js
@@ -1,18 +1,79 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
-import axios from "axios"
-
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const getAllUser = createAsyncThunk(
-  'user/getAllUser',
+  "user/getAllUser",
   async (rejectWithValue) => {
     try {
-      const response = await axios.get('/api/users')
-      const data = { data: response.data }
-      return data
+      const response = await axios.get("/api/users");
+      const data = { data: response.data };
+      return data;
     } catch (error) {
-      console.error(error)
-      return rejectWithValue({ data: error.response.data })
+      console.error(error);
+      return rejectWithValue({ data: error.response.data });
     }
-  },
-)
-export {getAllUser}
+  }
+);
+const followUser = createAsyncThunk(
+  "user/followUser",
+  async ({ _id, token }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `/api/users/follow/${_id}`,
+        {},
+        { headers: { authorization: token } }
+      );
+      const data = { data: response.data };
+      return data;
+    } catch (error) {
+      return rejectWithValue({ data: error.response.data });
+    }
+  }
+);
+
+const unfollowUser = createAsyncThunk(
+  "user/unfollowUser",
+  async ({ _id, token }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `/api/users/unfollow/${_id}`,
+        {},
+        { headers: { authorization: token } }
+      );
+      const data = { data: response.data };
+      return data;
+    } catch (error) {
+      return rejectWithValue({ data: error.response.data });
+    }
+  }
+);
+const getSingleUser = createAsyncThunk(
+  "user/getSingleUser",
+  async (username, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(`/api/users/${username}`);
+      const data = { data: response.data };
+      return data;
+    } catch (error) {
+      console.error(error);
+      return rejectWithValue({ data: error.response.data });
+    }
+  }
+);
+const editProfile = createAsyncThunk(
+  "user/editProfile",
+  async ({ userData, token }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        "/api/users/edit",
+        { userData },
+        { headers: { authorization: token } }
+      );
+      const data = { data: response.data };
+      return data;
+    } catch (error) {
+      return rejectWithValue({ data: error.response.data });
+    }
+  }
+);
+export { getAllUser, followUser, unfollowUser, editProfile, getSingleUser };
