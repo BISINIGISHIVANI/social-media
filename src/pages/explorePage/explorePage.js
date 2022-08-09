778a7f46ff1594b6904e71a60c34b99278cc6520
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-08-09 09:35:38 +0530

    refactor:add filter posts by trending,latest

--- src/pages/explorePage/explorePage.js
+++ src/pages/explorePage/explorePage.js
@@ -1,8 +1,9 @@
-import { Navbar,Sidebar,PostCard } from "../../components";
+import { Navbar, Sidebar, PostCard } from "../../components";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getAllPosts } from "../../redux/asyncThunk/postThunk";
-import { trendingPosts ,latestPosts} from "../../redux/slices/postSlice";
+import { trendingPosts, latestPosts } from "../../redux/slices/postSlice";
+import { SuggestUser } from "../../components/suggestUser/suggestUsers";
 const ExplorePage = () => {
   const dispatch = useDispatch();
   const { posts } = useSelector((state) => state.posts);
@@ -14,14 +15,24 @@ const ExplorePage = () => {
       <Navbar />
       <div className="flex-row">
         <div>
-          <Sidebar/>
+          <Sidebar />
         </div>
         <section className="flex-row main-content">
           <section className="content flex-col gap">
             <div className="flex-col gap">
               <div className="flex-row align-end flex-wrap gap">
-                <button className="post-modal-btn btn-secondary"onClick={()=>dispatch(trendingPosts())}>trending Posts</button>
-                <button className="post-modal-btn post-btn"onClick={()=>dispatch(latestPosts())}>latest Posts</button>
+                <button
+                  className="post-modal-btn btn-secondary"
+                  onClick={() => dispatch(trendingPosts())}
+                >
+                  trending Posts
+                </button>
+                <button
+                  className="post-modal-btn post-btn"
+                  onClick={() => dispatch(latestPosts())}
+                >
+                  latest Posts
+                </button>
               </div>
               {[...posts].reverse().map((post) => (
                 <PostCard key={post._id} post={post} />
@@ -29,7 +40,7 @@ const ExplorePage = () => {
             </div>
           </section>
           <aside className="padding-edges aside">
-           {/* suggetUser  */}
+            <SuggestUser />
           </aside>
         </section>
       </div>
